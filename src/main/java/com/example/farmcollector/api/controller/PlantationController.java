b14113858a8ded983d347c30f1e03c3774648f7f
Tharuka Herath <tharukaherath3201@gmail.com>
2024-05-14 14:52:18 +0530

    requestmapping for plantation

--- src/main/java/com/example/farmcollector/api/controller/PlantationController.java
+++ src/main/java/com/example/farmcollector/api/controller/PlantationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/details")
+@RequestMapping("/plantation")
 public class PlantationController {
     private final PlantationService plantationService;
 
